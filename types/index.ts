bd389dfc69e9b80e7a78502fa13c5e91e7bbae0a
atul khemka <atul.khemka@claimtherapist.com>
2024-01-22 13:10:48 +0530

    more fixes

--- types/index.ts
+++ types/index.ts
@@ -1,11 +1,11 @@
 // ====== USER PARAMS
 export type CreateUserParams = {
     clerkId: string
-    firstName: string
-    lastName: string
+    firstName?: string
+    lastName?: string
     username: string
     email: string
-    photo: string
+    photo?: string
   }
   
   export type UpdateUserParams = {
